5d98a3dcf7fb6e1e1af7d76dff3b427be7869aad
jk021227 <yk2606@nyu.edu>
2023-12-04 23:28:21 -0500

    bash script should be complete

--- Termolator/run_termolator_spanish.sh
+++ Termolator/run_termolator_spanish.sh
@@ -2,40 +2,42 @@
 # Date: November 28, 2023
 # Description: Script to run the Spanish version of the Termolator.
 # Usage Format: 
-# run_termolator_spanish.sh desired_output_name cleaned/foreground cleaned/background Termulator True
-
-# Usage example: bash run_Termolator_chinese.sh desired_output_name foreground directory, background directory Termolator directory
-# bash run_Termolator_chinese.sh results cleaned/background cleaned/foreground ..
+# bash run_Termolator_spanish.sh results cleaned/background cleaned/foreground .
+# $1 = desired_output_name
+# $2 = foreground directory
+# $3 = background directory
+# $4 = Termolator directory
 echo
 echo -e "Step 1 : Tagging using  Spanish POS tagger"
 # create directories for POS tagged files
-DIR=$2_tagged
+DIR=$1_tagged
 if [ -d "$DIR" ]; then
     rm -r $DIR
     echo "Old $DIR removed!"
 fi
-mkdir $2_tagged
-mkdir $2_tagged/background/
-mkdir $2_tagged/foreground/
+mkdir $1_tagged
+mkdir $1_tagged/background/
+mkdir $1_tagged/foreground/
 
 # Run Spanish Noun Chunker
-python3 $5/spanish_pos_tagger.py -input $2_cleaned/background/ -output $2_tagged/background
-python3 $5/spanish_pos_tagger.py -input $2_cleaned/foreground/ -output $2_tagged/foreground
+python3 $4/spanish_pos_tagger.py -input $1_cleaned/background/ -output $1_tagged/background
+python3 $4/spanish_pos_tagger.py -input $1_cleaned/foreground/ -output $1_tagged/foreground
 
 echo -e "Step 2 : Noun Chunker Generator\nGenerating .tchunk and .pos files for the distributional ranking..."
 # noun_chunker_generator.py implemented by Leizhen
-python3 $5/spanish_noun_chunker_generator.py -f $2_tagged/foreground -b $2_tagged/background -d $1 -p $5
+python3 $4/spanish_noun_chunker_generator.py -f $1_tagged/foreground -b $1_tagged/background #-d $1 -p $5
+
 echo
 
 echo -e "Step 3 : Distributional ranking\nGenerating .tchunk and .pos files for the distributional ranking...\n"
 # MEASURES = ['TFIDF', 'DRDC', 'KLDiv', 'Weighted'] , same as English version
-ls -1 output_foreground/ | grep "tchunk$" | awk '{print "output_foreground/"$1}' > $2.internal_foreground_tchunk_list
-ls -1 output_background/ | grep "tchunk$" | awk '{print "output_background/"$1}' > $2.internal_background_tchunk_list
-python3 $5/distributional_component.py NormalRank $2.internal_foreground_tchunk_list $2.all_terms False $2.internal_background_tchunk_list
+ls -1 output_foreground/ | grep "tchunk$" | awk '{print "output_foreground/"$1}' > $1.internal_foreground_tchunk_list
+ls -1 output_background/ | grep "tchunk$" | awk '{print "output_background/"$1}' > $1.internal_background_tchunk_list
+python3 $4/distributional_component.py NormalRank $1.internal_foreground_tchunk_list $1.all_terms False $1.internal_background_tchunk_list
 
 # echo -e "Step 4 : Accessor Variety Filter\nFiltering all terms obtained previously...\n"
 # python3 $5/accessorvariety.py $2.all_terms foreground_tchunk_list > $2.AV_filtered_terms
 # echo -e "All steps completed! Final output file: $2.AV_filtered_terms"
 
-cut -f 1 $2.all_terms > $2.out_term_list
+cut -f 1 $1.all_terms > $1.out_term_list
 
