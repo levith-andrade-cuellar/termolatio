d79b25c0ed1d8db782fc70bd0a5fb21cc3ba3c1f
Pauline Wee <paulinekowee@gmail.com>
2023-12-07 22:13:21 -0500

    cli is run_cli.sh

---
+++ Termolator/run_cli.sh
@@ -0,0 +1,27 @@
+# command line interface (cli) for spanish termolator
+# this calls ./run_termolator_spanish.sh 
+
+# Usage Format:
+# ./run_cli.sh 
+
+# reach goal: allow users to input url for foreground and background to scrape
+
+echo -e "Welcome to Spanish Termolator!\n"
+echo -e "Please specify your folder arguments below.\n"
+
+# Prompt the user for input with default values
+read -p "Enter results folder (enter for default): " results
+results=${results:-results}
+
+read -p "Enter cleaned background folder (enter for default): " background
+background=${background:-cleaned/background}
+
+read -p "Enter cleaned foreground folder (enter for default): " foreground
+foreground=${foreground:-cleaned/foreground}
+
+read -p "Enter Termolator folder (enter for default): " termolator
+termolator=${termolator:-.}
+
+echo -e "Input complete"
+# Run the command with user input
+./run_termolator_spanish.sh "$results" "$background" "$foreground" "$termolator"
--- Termolator/run_termolator_spanish.sh
+++ Termolator/run_termolator_spanish.sh
@@ -3,12 +3,14 @@
 # Authors: Pauline Wee, Jhon Kim, Levith Andrade Cuellar 
 # Date: November 28, 2023
 # Description: Script to run the Spanish version of the Termolator.
+
 # Usage Format: 
 #  ./run_termolator_spanish.sh results cleaned/background cleaned/foreground .
 # $1 = desired_output_name
 # $2 = foreground directory
 # $3 = background directory
 # $4 = Termolator directory
+
 echo
 echo -e "Step 1 : Tagging using  Spanish POS tagger"
 # create directories for POS tagged files
@@ -50,7 +52,7 @@ done
 
 echo -e "Step 2 : Noun Chunker Generator\nGenerating .tchunk and .pos files for the distributional ranking..."
 # noun_chunker_generator.py implemented by Leizhen
-python $4/spanish_noun_chunker_generator.py -f $1_tagged/foreground -b $1_tagged/background -p $4 #-d $1 -p $5 <- $1 is for chinese dict & $5 for text file chinese1.txt
+python3 $4/spanish_noun_chunker_generator.py -f $1_tagged/foreground -b $1_tagged/background -p $4 #-d $1 -p $5 <- $1 is for chinese dict & $5 for text file chinese1.txt
 
 echo
 
@@ -58,6 +60,6 @@ echo -e "Step 3 : Distributional ranking\nGenerating .tchunk and .pos files for 
 # MEASURES = ['TFIDF', 'DRDC', 'KLDiv', 'Weighted'] , same as English version
 ls -1 output_foreground/ | grep "tchunk$" | awk '{print "output_foreground/"$1}' > $1.internal_foreground_tchunk_list
 ls -1 output_background/ | grep "tchunk$" | awk '{print "output_background/"$1}' > $1.internal_background_tchunk_list
-python $4/distributional_component.py NormalRank $1.internal_foreground_tchunk_list $1.all_terms False $1.internal_background_tchunk_list
+python3 $4/distributional_component.py NormalRank $1.internal_foreground_tchunk_list $1.all_terms False $1.internal_background_tchunk_list
 cut -f 1 $1.all_terms > $1.out_term_list
 
